fb68ed310863e4eab0d6d4f869ac2ff93b8643d4
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-11 23:26:42 +0530

    -

--- practice express/server.js
+++ practice express/server.js
@@ -1,11 +1,9 @@
 const express = require("express")
+const bodyParser = require("body-parser");
 const app = express()
-const bodyParser = require("body-parser")
 
-app.listen(3000,function(req,res)
-{
-  console.log("Server Started at Port 3000");
-});
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", function(req,res){
 
@@ -15,7 +13,12 @@ app.get("/", function(req,res){
 
 app.post("/",function(req,res)
 {
-  var num = Number(req.body.number1);
+  var num = Number(req.body.height);
   var result = num*num;
   res.send("Square of number is "+result);
+});
+
+app.listen(3000,function(req,res)
+{
+  console.log("Server Started at Port 3000");
 });
