b54120a6aba5860c29ac6ff9bbbc782aa165245b
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-17 00:47:11 +0530

    -

--- API-WEBSITE/index.js
+++ API-WEBSITE/index.js
@@ -1,18 +1,43 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
+const crypto = require('crypto-js');
 const ba = require('bitcoinaverage');
-var publicKey = 'MjVmMDdiMTU5ZmQ4NDliMjgyY2IxN2YyYTQ1ODI3YjE';
+var public_Key = 'MjVmMDdiMTU5ZmQ4NDliMjgyY2IxN2YyYTQ1ODI3YjE';
 var secretKey = 'NzgwOGY5ZGY4MDE4NDBhZGEyMGU1MWM1MDVjODljODAyZjZmOTNhZmI5YTE0M2M0ODJkYzM4MzJmNDcxNzVlZA==';
-var restClient = ba.restfulClient(publicKey, secretKey);
-var wsClient =  ba.websocketClient(publicKey, secretKey);
+// var restClient = ba.restfulClient(public_Key, secretKey);
+// var wsClient =  ba.websocketClient(publicKey, secretKey);
+
 const app = express()
+var price
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(req,res)
 {
   console.log("Server running at port 3000")
 });
+
+
+
+
+app.post("/",function callback(req,res){
+
+var symbol     =  req.body.crypto + req.body.fiat;
+
+
+var ticker_btcusd_url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/'+symbol;
+
+var options = {
+    url: ticker_btcusd_url,
+    headers: {
+        'x-ba-key':'MjVmMDdiMTU5ZmQ4NDliMjgyY2IxN2YyYTQ1ODI3YjE' 
+    }
+};
+request(options, callback);
+});
+
+
+
 
 
 // app.post("/",function(req,res)
@@ -28,36 +53,36 @@ app.listen(3000, function(req,res)
 
 //
 // });
-
-app.post("/",function(req,res)
-{
-
-      var symbol_set = 'global';
-      var symbol     =  req.body.crypto + req.body.fiat;
-
-      restClient.getTickerDataPerSymbol(symbol_set, symbol,
-
-        function(res) {
-
-
-                   var price = JSON.parse(res)
-                   // console.log(price)
-                   console.log(price.ask)
-                   
-
-        //            },
-        // function(error){
-        //          console.log(error);
-        }) ;
-
-
-
-
-
-
-});
-
-
+// -------------------------------------------
+// app.post("/",function(req,res)
+// {
+//
+//       var symbol_set = 'global';
+//       var symbol     =  req.body.crypto + req.body.fiat;
+//
+//       restClient.getTickerDataPerSymbol(symbol_set, symbol,
+//
+//         function(res) {
+//
+//
+//                   price = JSON.parse(res)
+//                    // console.log(price)
+//                    console.log(price.ask)
+//
+//
+//         //            },
+//         // function(error){
+//         //          console.log(error);
+//         }) ;
+//
+//
+//
+//
+//
+//
+// });
+//
+// -----------------------------
 
 
 app.get("/",function(req,res)
