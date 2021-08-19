faf6999233bd1a12ba9e48d4ef612b787f430924
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-20 00:37:06 +0530

    -

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,59 +1,73 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const http = require('http')
-
-const app = express()
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+const https = require("https");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended : true}))
 
-app.get("/",function(req,res)
-{
-  res.sendFile(__dirname+"/signup.html");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res)
+
+app.post("/failure",function(req,res)
 {
+  res.redirect("/")
+})
+app.post("/", function (req, res) {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
   const email = req.body.email;
-  console.log(firstName,lastName,email)
 
-  var data ={
-    members : [
-
+  const data = {
+    members: [
       {
-        email_address : "mil10rodrigz@gmail.com",
-        status : "subscribed",
+        email_address: email,
+        status: "subscribed",
         merge_fields: {
-          FNAME : "Milton",
-          LNAME : "Rodrigues"
+          FNAME: firstName,
+          LNAME: lastName,
         }
-
       }
     ]
   };
 
-  var jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
+  const url = "https://us5.api.mailchimp.com/3.0/lists/9c75755e3e"
+  const options = {
+    method: "POST",
+    auth: "rodriguesmilton21:2c11bc941efe69fc3bcdc1b13984f568-us5",
+  };
 
 
-https.request(url,options, function(response)
-{
+  const request = https.request(url, options, function (response) {
 
-})
+      if (response.statusCode == 200)
+      {
+        res.sendFile(__dirname+"/success.html");
 
-})
+      }
+      else
+      {
+        res.sendFile(__dirname+"/failure.html");
+      }
 
-curl -X POST \
-  'https://${dc}.api.mailchimp.com/3.0/lists/{list_id}?skip_merge_validation=<SOME_BOOLEAN_VALUE>&skip_duplicate_check=<SOME_BOOLEAN_VALUE>' \
-  --user "anystring:${apikey}"' \
-  -d '{"members":[],"update_existing":false}'
+      response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 
-app.listen(3000, function(req,res)
-{
-  console.log("Server running at port 3000")
-})
+  console.log(firstName, lastName, email);
+});
 
-// 2c11bc941efe69fc3bcdc1b13984
+app.listen(3000, function () {
+  console.log("Port is running on port 3000");
+});
+
+
+// 2c11bc941efe69fc3bcdc1b13984f568-us5
 // 9c75755e3e
