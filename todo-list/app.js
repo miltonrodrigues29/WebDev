ba6bd1d53a4aa12b5b9c4ed08c4055118cabeee3
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-23 23:55:07 +0530

    To-Do with Header and Footer layouts

--- todo-list/app.js
+++ todo-list/app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 const ejs = require("ejs");
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
