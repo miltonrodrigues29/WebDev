d0f7939e6842b88274d955cb264e54f5043c123d
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-26 23:52:22 +0530

    Rendering data from MongoDB to EJS template complete :)

---
+++ todo-list-v2/app.js
@@ -0,0 +1,101 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+
+const itemsSchema = {
+  name: String
+}
+
+Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add  a new item"
+});
+
+const item3 = new Item({
+  name: "<---Hit this to delete an Item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length == 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log("ERROR OCCURED");
+        } else {
+          console.log("Successfully Inserted the elements");
+        }
+
+      });
+      res.redirect("/");
+    } else {
+      console.log(foundItems);
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+
+
+  });
+
+
+
+});
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
