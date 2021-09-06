1ddd9f775f07803ce460e74c3d4db792f2454d7c
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-09-06 20:57:44 +0530

    post request from postman complete

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -2,37 +2,81 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
+
 const app = express();
 
 
 mongoose.connect('mongodb://localhost:27017/wikiDB');
+app.use(express.static("public"));
 app.listen(3000, function()
 {
   console.log("Server running at port 300")
 });
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-const wikiSchema =
+const articleSchema =
 {
   title: String,
   content: String
 }
 
-Item = mongoose.model("Wiki",wikiSchema);
+const Article = mongoose.model("Article",articleSchema);
 
 
-app.get("/",function(req,res)
+app.get("/articles",function(req,res)
 {
-  const item1 = Item({
-    title: "Test 1",
-    content: "Test 1 content"
+  Article.find({},function(err,foundArticles)
+  {
+    if(!err)
+    {
+        res.send(foundArticles);
+    }
+    else
+    {
+      res.send(err);
+    }
+
+  });
+
+//   const item1 = Article({
+//     title: "Test 1",
+//     content: "Test 1 content"
+//   })
+//
+//   item1.save(function(error)
+// {
+//   if(!error)
+//   {
+//     console.log("Item Saved Successfully");
+//   }
+// })
+
+});
+
+// -------------------------------------------------------------------------------------
+
+// POST METHODS
+
+app.post("/articles",function(req,res)
+{
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
   })
-  
-  item1.save(function(error)
+  newArticle.save(function(error)
 {
   if(!error)
   {
-    console.log("Item Saved Successfully");
+    res.send("Data Stored Successfully!");
   }
-})
-
+  else
+  {
+    res.render("Oops, Error Occured");
+  }
 });
+})
